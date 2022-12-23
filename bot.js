d41cd0dac3614b3730a2998b93a01d4939bc1338
tcooi <12738325+tcooi@users.noreply.github.com>
2022-12-23 15:17:30 +0100

    update dependencies and refactor

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const { prefix } = require('./config.json');
 const fs = require('fs');
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds]});
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 const token = process.env.DISCORD_TOKEN;
@@ -13,35 +13,48 @@ console.log(`Loading ${commandFiles.length} commands`);
 commandFiles.forEach((file, i) => {
   let command = require(`./commands/${file}`);
   console.log(`${i+1}. ${file}`);
-  client.commands.set(command.name, command)
-});
-
-client.on('ready', async () => {
-  console.log(`\n${client.user.tag} is online`);
-
-  try {
-    let link = await client.generateInvite(['ADMINISTRATOR']);
-    console.log(`Use this link to invite the bot: ${link}`);
-  } catch (error) {
-    console.error(error);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command)
+  } else {
+    console.log(`command ${file} is missing data or execute`);
   }
 });
 
-client.on('message', message => {
-  if (message.author.bot || !message.content.startsWith(prefix)) return;
+client.once(Events.ClientReady, c => {
+  console.log(`\n${c.user.tag} is online`);
 
-  let args = message.content.slice(prefix.length).split(' ');
-  let commandName = args.shift().toLowerCase();
+  // try {
+  //   let link = client.generateInvite({
+  //     permissions: [
+  //       PermissionFlagsBits.SendMessages,
+  //       PermissionFlagsBits.ManageGuild,
+  //       PermissionFlagsBits.MentionEveryone
+  //     ],
+  //     scopes: [OAuth2Scopes.Bot],
+  //   });
+  //   console.log(`Use this link to invite the bot: ${link}`);
+  // } catch (error) {
+  //   console.error(error);
+  // }
+});
 
-  let command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-  if (!command) return;
+client.on(Events.InteractionCreate, interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('There was an error trying to execute that command')
-  }
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  // let args = message.content.slice(prefix.length).split(' ');
+  // let commandName = args.shift().toLowerCase();
+
+  // let command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  // if (!command) return;
+
+  // try {
+  //   command.execute(message, args);
+  // } catch (error) {
+  //   console.error(error);
+  //   message.reply('There was an error trying to execute that command')
+  // }
 });
 
 client.login(token);
